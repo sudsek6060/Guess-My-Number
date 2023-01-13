ed4973b3f1a8174f9523a56cbff92a49e5b309a9
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-01-13 06:50:03 +0530

    Again functionality

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ document.querySelector('.check').addEventListener('click', function(){
         }
     }
 });
+
+document.querySelector('.againn').addEventListener('click', function () {
+    score = 20;
+    // secreteNumber = Math.trunc(Math.random() * 20) + 1;
+  
+    document.querySelector('.message').textContent = 'Start guessing...';
+    // displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+  
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
+  
